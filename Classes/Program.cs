80fff10c3887fb618eadb810836b831019b6b559
Mardixx <kimi.lefort.pro@gmail.com>
2023-12-23 18:48:18 +0100

    getting started

---
+++ Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NameBankAccount
+{
+    class BankAccount 
+    {
+        static void Main(string[] args)
+        {
+            private int myVar;
+
+            public int MyProperty
+            {
+                get { return myVar; }
+                set { myVar = value; }
+            }
+            public BankAccount(int myProperty)
+            {
+                this.MyProperty = myProperty;
+            }
+            public static void Func()
+            {
+                Console.WriteLine("Hey it works");
+            }
+        }
+    }
+}
